4956ac5194476d9aa9f5494b8fb15086ea18219e
mseyli <159932756+amvitality@users.noreply.github.com>
2025-01-02 23:59:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('revealButton').addEventListener('click', () => {
+    const surpriseMessage = document.getElementById('surpriseMessage');
+    surpriseMessage.classList.remove('hidden');
+});
